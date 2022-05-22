868f05bd478e9fb9b635f53c270df41e8a50fb0d
Miguel Angel Mena <menatabor09@gmail.com>
2022-05-21 22:34:34 -0500

    cambios para las pruebas faltantes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
-export const GifExpertApp = () => {
+export const GifExpertApp = ({defaultCategories=[]}) => {
 
-    const [categories, setCategories] = useState(['Pilsen']);
+    const [categories, setCategories] = useState(defaultCategories);
 
     return (
         <>
